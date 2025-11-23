f7060facd6a0a02d074a94f3cacd4b7aae01e616
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2025-11-23 17:55:00 -0300

    upgrades added pt.1

--- store/store.gd
+++ store/store.gd
@@ -17,20 +17,35 @@ var itens_list = [
 	preload("res://books_datas/book13.tres"),
 	preload("res://books_datas/book14.tres"),
 	preload("res://books_datas/book15.tres"),
-	preload("res://books_datas/book16.tres")
+	preload("res://books_datas/book16.tres"),
+	preload("res://upgrades_datas/upgrade0.tres"),
+	preload("res://upgrades_datas/upgrade1.tres"),
+	preload("res://upgrades_datas/upgrade2.tres"),
+	preload("res://upgrades_datas/upgrade3.tres"),
+	preload("res://upgrades_datas/upgrade4.tres"),
+	preload("res://upgrades_datas/upgrade5.tres"),
+	preload("res://upgrades_datas/upgrade6.tres"),
+	preload("res://upgrades_datas/upgrade7.tres"),
+	preload("res://upgrades_datas/upgrade8.tres"),
+	preload("res://upgrades_datas/upgrade9.tres"),
+	preload("res://upgrades_datas/upgrade10.tres"),
+	preload("res://upgrades_datas/upgrade11.tres"),
+	preload("res://upgrades_datas/upgrade12.tres"),
+	preload("res://upgrades_datas/upgrade13.tres"),
+	preload("res://upgrades_datas/upgrade14.tres"),
+	preload("res://upgrades_datas/upgrade15.tres")
 ]
 
 var has_products = false
  
 func _ready() -> void:
-	for i in 7:
-		if randf_range(0, 1) > 0.1:
-			var marker: Marker2D = $StoreBackground.get_child(randi_range(0, 3))
-			if marker == null or marker.get_child_count() > 0: return
-			var item = load("res://store_slot/store_slot.tscn").instantiate()
-			item.item_to_sell = itens_list[randi_range(0, itens_list.size()-1)]
-			item.seller = $SellerNPC
-			marker.add_child(item)
+	for i in 14:
+		var marker: Marker2D = $StoreBackground.get_child(randi_range(0, 6))
+		if marker == null or marker.get_child_count() > 0: return
+		var item = load("res://store_slot/store_slot.tscn").instantiate()
+		item.item_to_sell = itens_list[randi_range(0, itens_list.size()-1)]
+		item.seller = $SellerNPC
+		marker.add_child(item)
 	has_products = true
 
 func _process(_delta: float) -> void:
--- upgrades_datas/upgrade_data.gd
+++ upgrades_datas/upgrade_data.gd
@@ -2,6 +2,7 @@ extends Resource
 class_name UpgradeData
 
 @export var upgrade_name: String = ""
+@export var upgrade_description: String = ""
 @export_enum("extra_damage", "extra_bullet_speed", "extra_attack_speed", "extra_bullet_size") var upgrade_type: String = ""
 @export var upgrade_value: int = 1
 @export var upgrade_image: CompressedTexture2D = null
